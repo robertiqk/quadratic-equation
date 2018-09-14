fbb7c7f240fe077bff2eab073c028d2d2e45e1bd
robertiqk <delux1945@gmail.com>
2018-09-14 22:26:52 +0300

    Exercise 4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+  let arr = equation.split(' ');
+  let resultsArr = [];
+  let X1;
+  let X2;
+  let D;
+  if(arr[7] == "-") {
+    arr[8] = -arr[8]
+  }
+  D = arr[4] * arr[4] - 4 * arr[0] * arr[8];
+  if ( D < 0 ) {
+    D = -Math.sqrt(Math.abs(D));
+    if (arr[3] == "-") { 
+      X1 = Math.round((+arr[4] + Math.round(D)) / (2 * arr[0])  );
+      X2 = Math.round((+arr[4] - Math.round(D)) / (2 * arr[0])  );
+    } else {
+      X1 = Math.round((-arr[4] + Math.round(D)) / (2 * arr[0]) );
+      X2 = Math.round((-arr[4] - Math.round(D)) / (2 * arr[0]) );
+    }
+  } else {
+    if (arr[3] == "-") {
+      X1 = Math.round((+arr[4] + Math.round(Math.sqrt(D))) / (2 * arr[0]) );
+      X2 = Math.round((+arr[4] - Math.round(Math.sqrt(D))) / (2 * arr[0]) );
+    } else{
+      X1 = Math.round((-arr[4] + Math.round(Math.sqrt(D))) / (2 * arr[0]) );
+      X2 = Math.round((-arr[4] - Math.round(Math.sqrt(D))) / (2 * arr[0]) );
+      }
+    }
+  resultsArr.push(X1, X2);
+  resultsArr.sort(function(a, b){
+    return a - b;
+  });
+  
+  return resultsArr;
+} 
